f7d66a4c679828575f41e369d80ef5c660c3daa5
wzaoesm <wzaoesm@proton.me>
2025-06-09 18:34:39 +0700

    Warehouse System SQL first commit (pakai sqlite dan output di terminal masih sedikit kotor)

---
+++ logic_nolep/warehouse_system_sql/index.js
@@ -0,0 +1,28 @@
+const { 
+    initializeWarehouseDatabase,
+    insertProducts,
+    showProducts,
+    insertInventory,
+    showInventoryDetails,
+    updateProductPrice,
+    showWarehouseInventoryValue,
+    insertOrdersAndDetails,
+    showOrderTotals,
+    showUnorderedProducts,
+    showCurrentStockLevels
+} = require('./query.js');
+
+// Panggil fungsi inisialisasi database terlebih dahulu
+// initializeWarehouseDatabase();
+
+// Uncomment satu per satu fungsi yang ingin Anda jalankan
+// insertProducts();
+// showProducts();
+// insertInventory();
+// showInventoryDetails();
+// updateProductPrice();
+// showWarehouseInventoryValue();
+// insertOrdersAndDetails();
+// showOrderTotals();
+// showUnorderedProducts();
+// showCurrentStockLevels();
